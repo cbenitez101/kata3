1d703e88f448a0080dbc4eefefe263fc4efb2954
Servibyte <Servibyte@Servibyte-PC>
2016-11-07 17:27:29 +0000

    Commit3 del kata3 v1 2016 - 2017 Se añade clase Hisogram

--- src/kata3/Histogram.java
+++ src/kata3/Histogram.java
@@ -7,7 +7,9 @@ import java.util.HashMap;
  * @author Servibyte
  */
 public class Histogram {
+    
     HashMap<String, Integer> histograma= new HashMap<>();
+    
     public void increment(String clave){
         if (histograma.containsKey(clave)) histograma.put(clave, histograma.get(clave) + 1);
         else histograma.put(clave, 1);
